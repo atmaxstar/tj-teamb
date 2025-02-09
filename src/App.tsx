22b029d4eef8623a5dfd826d290f2f34c26c987d
atmaxstar <haniyasusin@gmail.com>
2025-02-09 12:31:36 +0900

    不具合を修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Home from './Home'
+import CalorieWeightChart from './CaloriesWeightChart'
 
 function App() {
   const [page, setPage] = useState<"home"|"dashboard">("home")
